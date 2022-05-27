d5243dcdce62d5069b73c13f12149fb118bf84f6
fsaythanry <aldyzkobf@gmail.com>
2022-05-27 21:24:21 +0800

    refaktor: adjust login validation

--- app/Views/admin/auth/login.php
+++ app/Views/admin/auth/login.php
@@ -3,7 +3,7 @@
 <?= $this->section('content') ?>
 <div class="row justify-content-center">
     <div class="col-md-4 bg-body rounded shadow-sm my-3 px-4 py-5">
-        <form action="<?= route_to('postLogin') ?>" method="post">
+        <form action="<?= route_to('postLogin') ?>" class="needs-validation" method="post" novalidate>
             <div class="form-floating mb-3 ">
                 <input type="text" class="form-control" id="username" placeholder="username" name="username" autocomplete="new" required autofocus>
                 <label for="username">Username</label>
