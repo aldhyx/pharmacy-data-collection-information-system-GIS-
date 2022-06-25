0031873422d41d7949be44bfa9ea0dd3ebd47672
fsaythanry <aldyzkobf@gmail.com>
2022-06-26 01:52:39 +0800

    add show pharmacies by district

--- app/Controllers/Admin/HomeController.php
+++ app/Controllers/Admin/HomeController.php
@@ -18,12 +18,13 @@ class HomeController extends BaseController
         $did = $this->request->getVar('did');
         if ($did) {
             $districtData = $this->model->getWithTotalPharmacies($did)->getResultArray();
+            $pharmacies = $this->modelPharmacies->where('id_districts', $did)->get()->getResultArray();
         } else {
             $districtData = $this->model->getWithTotalPharmacies()->getResultArray();
+            $pharmacies = $this->modelPharmacies->findAll();
         }
 
         $districts = $this->model->findAll();
-        $pharmacies = $this->modelPharmacies->findAll();
 
         return view('admin/home/home', [
             'page' => 'home',
